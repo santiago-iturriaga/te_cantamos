016b8880b5a6d364b8dacc96990feed7d0884d20
Santiago Iturriaga <siturria@fing.edu.uy>
2021-03-30 19:12:27 -0300

    Cambio a kotlin

---
+++ app/src/main/java/edu/neurodesarrollomusical/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package edu.neurodesarrollomusical.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
