b23c72a9924db56f6517ba75d22f06b82a16f3a3
Santiago Iturriaga <siturria@fing.edu.uy>
2021-03-30 19:37:43 -0300

    borrado de login y lista

--- app/src/main/java/edu/neurodesarrollomusical/ui/login/LoginResult.kt
+++
@@ -1,9 +0,0 @@
-package edu.neurodesarrollomusical.ui.login
-
-/**
- * Authentication result : success (user details) or error message.
- */
-data class LoginResult(
-    val success: LoggedInUserView? = null,
-    val error: Int? = null
-)
