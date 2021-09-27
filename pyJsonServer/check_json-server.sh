1f7e2b55908ab3e36ee11c054af9761aa9e7fe5a
Santiago Iturriaga <santiago.iturriaga@gmail.com>
2021-09-27 19:26:28 -0300

    agrego cron check

--- pyJsonServer/check_json-server.sh
+++ pyJsonServer/check_json-server.sh
@@ -3,4 +3,6 @@
 kpid=$(cat /tmp/json-server.pid)
 echo $kpid
 
-[ -n "${kpid}" -a -d "/proc/${kpid}" ] && echo "process exists" || echo "process not exists"
+#[ -n "${kpid}" -a -d "/proc/${kpid}" ] && echo "process exists" || echo "process not exists"
+
+[ -n "${kpid}" -a -d "/proc/${kpid}" ] || curl -s --form-string "token=a4eqquv5vnpu86kxe5jv668vwoa77s" --form-string "user=ut7o7uwy569ob7p5dvjqmya54rd5kk" --form-string "message=Error con mama_te_canta server!" --form-string "priority=1" https://api.pushover.net/1/messages.jsonZ
