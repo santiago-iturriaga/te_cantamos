477a3dff6586b3a4a7f6fa495e0bcba2ef9be76c
Santiago Iturriaga <santiago.iturriaga@gmail.com>
2021-10-26 09:36:13 -0300

    fix daemon

--- pyJsonServer/run_json-server.sh
+++ pyJsonServer/run_json-server.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-#LOG_PATH=/archivos/mama_te_canta
-LOG_PATH=/tmp
-python3 json-server.py &> $LOG_PATH/json-server.log &
+EXEC_PATH=/archivos/mama_te_canta/neurodesarrollomusical/pyJsonServer
+#EXEC_PATH=/tmp
+
+LOG_PATH=/archivos/mama_te_canta
+#LOG_PATH=/tmp
+
+#export PATH=/sbin:/usr/sbin:/bin:/usr/bin:/usr/local/sbin:/usr/local/bin
+#umask 022
+cd /
+nohup setsid $0 $EXEC_PATH/json-server.py 2>$LOG_PATH/json-server.err >$LOG_PATH/json-server.out &
 echo "$!" > /tmp/json-server.pid
